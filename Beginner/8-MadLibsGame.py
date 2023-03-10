003460e7b64d25769be28ebf437d74fe65213cb6
Knockturn <pkbrauner@gmail.com>
2023-03-10 19:44:45 +0100

    madlibs done

--- Beginner/8-MadLibsGame.py
+++ Beginner/8-MadLibsGame.py
@@ -1,1 +1,9 @@
 # https://youtu.be/rfscVS0vtbw?t=3507
+print("Welcome to Mad Libs Game!")
+print("Start below:")
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
