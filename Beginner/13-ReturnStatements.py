f75d9970e8c199990511297ff612d277ce5d6d9b
Knockturn <pkbrauner@gmail.com>
2023-03-12 15:25:19 +0100

    returns done

--- Beginner/13-ReturnStatements.py
+++ Beginner/13-ReturnStatements.py
@@ -1,1 +1,10 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=5651s
+
+def cube(num:int): # cube is power of 3 (3*3*3. 3^3)
+    result = num*num*num # result is a local variable
+    return result # return statement. Return exits the function
+
+print(cube(3)) # 27
+
+my_result = cube(4) # 64. Store the result in a variable
+print(my_result)
