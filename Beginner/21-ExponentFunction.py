e30ed0204bcfad97b5ed5481977ca397658824ac
Knockturn <pkbrauner@gmail.com>
2023-03-12 17:22:43 +0100

    exponent function done

--- Beginner/21-ExponentFunction.py
+++ Beginner/21-ExponentFunction.py
@@ -1,1 +1,13 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=9680s
+
+# Exponent Function (2 ** 3 = 2 * 2 * 2) (2 to the power of 3)
+
+print(2 ** 3) # 2 to the power of 3 (2 * 2 * 2)
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for i in range(pow_num):
+        result = result * base_num
+    return result
+
+print(f"Raise: {raise_to_power(2, 3)}") # 2 to the power of 3 (2 * 2 * 2)
