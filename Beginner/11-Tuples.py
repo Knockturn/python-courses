ab23643aed6b2d3abad29e14e4599467943f0c25
Knockturn <pkbrauner@gmail.com>
2023-03-12 15:07:05 +0100

    Tuples done

--- Beginner/11-Tuples.py
+++ Beginner/11-Tuples.py
@@ -1,1 +1,7 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=4737s
+
+coordinates = (4, 5) # Tuples are immutable (cannot be changed once created)
+print(coordinates) # Print the tuple
+print(coordinates[0]) # Access the tuple
+
+# Tuples are used to store data that should not be changed!
