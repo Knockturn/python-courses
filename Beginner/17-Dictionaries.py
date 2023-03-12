1ac169610b4a4b811bff060159c85625760fc5af
Knockturn <pkbrauner@gmail.com>
2023-03-12 17:08:03 +0100

    dictionary and while done

--- Beginner/17-Dictionaries.py
+++ Beginner/17-Dictionaries.py
@@ -1,1 +1,22 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=7637s
+
+# Dictionaries are used to store data values in key:value pairs.
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions["Nov"]) # prints November.
+print(monthConversions.get("Dec")) # prints December.
+print(monthConversions.get("Luv", "Not a valid key"))
--- Beginner/18-WhileLoop.py
+++ Beginner/18-WhileLoop.py
@@ -1,1 +1,9 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=8053s
+
+# While loops are used to execute a set of statements as long as a condition is true.
+
+i = 0
+while i <= 9:
+    print(i)
+    i += 1
+print("Done with loop")
