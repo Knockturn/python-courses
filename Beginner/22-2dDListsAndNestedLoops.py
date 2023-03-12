29d295af0d477e69ad7940fe5c0413035baf26d8
Knockturn <pkbrauner@gmail.com>
2023-03-12 22:27:50 +0100

    2d lists and nested for loop

--- Beginner/22-2dDListsAndNestedLoops.py
+++ Beginner/22-2dDListsAndNestedLoops.py
@@ -1,1 +1,16 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=10033s
+
+number_grid = [ # 2D List. Sort of like a grid of numbers
+    [0, 1],
+    [2, 3],
+    [4, 5],
+    [6, 7],
+    [8, 9]
+]
+
+print(number_grid[0][1]) # Prints 1
+
+for row in number_grid: # Loops each "row"
+    print(f"row: {row}")
+    for col in row: # Loops each "column"
+        print(f"col: {col}")
