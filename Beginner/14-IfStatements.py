d5a99053bf0db93419d987057c9f3fc4001c8218
Knockturn <pkbrauner@gmail.com>
2023-03-12 15:49:16 +0100

    if statements done

--- Beginner/14-IfStatements.py
+++ Beginner/14-IfStatements.py
@@ -1,1 +1,20 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=6006s
+
+is_male = True
+is_tall = True
+
+if is_male:
+    print("You are a male")
+else:
+    print("You are a female")
+
+if is_male & is_tall: # & is the same as 'and'
+    print("You are a tall male!")
+elif is_male & ~is_tall: # ~ is the same as 'not'
+    print("You are a short male!")
+
+if is_male | is_tall: # or is the same as 'or'
+    print("You are a male, or tall, or both!")
+else:
+    print("You are neither male nor tall!")
+
--- Beginner/15-IfStatements&Comparisons.py
+++ Beginner/15-IfStatements&Comparisons.py
@@ -1,1 +1,18 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=6847s
+
+def max_number(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3: # > might be better than >=
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+print(f"Function max: {max_number(3, 4, 5)}")
+
+print(f"Built in max: {max(3, 4, 5)}") # built-in function tat does the same thing
+
+# <= is the same as 'less than or equal to'
+# >= is the same as 'greater than or equal to'
+# == is the same as 'equal to'
+# != is the same as 'not equal to'
