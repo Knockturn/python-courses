c3185366cd5a208aca4b0cdd06a4a7b3099b6eaf
Knockturn <pkbrauner@gmail.com>
2023-03-12 15:19:29 +0100

    functions done

--- Beginner/12-Functions.py
+++ Beginner/12-Functions.py
@@ -1,1 +1,10 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=5055s
+
+# say_hi("P", 36) # NameError: name 'say_hi' is not defined
+
+def say_hi(name, age):
+    print(f"Hello {name}, you are {age}")
+
+print("Above the function call")
+say_hi("Mike", 35)
+print("Below the function call")
