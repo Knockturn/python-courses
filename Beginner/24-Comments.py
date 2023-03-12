1526cfffc0f32515cd662dce8967dab34495c4f2
Knockturn <pkbrauner@gmail.com>
2023-03-12 22:37:13 +0100

    comments explained

--- Beginner/24-Comments.py
+++ Beginner/24-Comments.py
@@ -1,1 +1,14 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=10818s
+
+# Comments are lines of code that are ignored by the interpreter
+print("Comments are fun!")
+# print("This line won't print")
+
+# Great for documentation of code.
+
+'''
+Multi-line comments
+are done like this
+but they are not as common
+Python recommends using # for each line
+'''
