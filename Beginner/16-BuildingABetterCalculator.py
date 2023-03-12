aebca27c3f0267340d85dcbaf964a6d1aee2a94f
Knockturn <pkbrauner@gmail.com>
2023-03-12 15:54:22 +0100

    better calc done

--- Beginner/16-BuildingABetterCalculator.py
+++ Beginner/16-BuildingABetterCalculator.py
@@ -1,1 +1,16 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=7237s
+
+num1 = float(input("Enter 1st number: "))
+operator = input("Enter operator: ")
+num2 = float(input("Enter 2nd number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
