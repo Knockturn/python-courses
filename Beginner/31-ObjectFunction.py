80383768dd5969f13a7e2f8780b32f9029eb28cf
Philippe Barka Kjellberg Bräuner <pkb@solar.dk>
2023-03-13 11:54:34 +0100

    object functions done

--- Beginner/31-ObjectFunction.py
+++ Beginner/31-ObjectFunction.py
@@ -1,1 +1,9 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=14908s
+
+from assets.student import Student # See .\assets\student.py
+
+student1 = Student("Jim", 23, "Business", 3.1, False)
+student2 = Student("Pam", 54, "Art", 3.8, False)
+
+print(f"Student1 is on honor roll: {student1.on_honor_roll()}") # This is how you call a function from a class
+print (f"Student2 is on honor roll: {student2.on_honor_roll()}")
--- Beginner/assets/student.py
+++ Beginner/assets/student.py
@@ -1,4 +1,4 @@
-# class for 29-ClassesAndObjects.py
+# class for 29-ClassesAndObjects.py & 31-ObjectFunction.py
 class Student:
     def __init__(self, name, age, major, gpa, is_on_probation):
         self.name = name
@@ -6,3 +6,11 @@ class Student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
+
+
+    # Below created in 31-ObjectFunction.py
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
