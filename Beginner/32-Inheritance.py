256e9f1c4569a8138e3476b43846efc4bed3cab3
Knockturn <pkbrauner@gmail.com>
2023-03-13 18:21:12 +0100

    32 done

--- Beginner/32-Inheritance.py
+++ Beginner/32-Inheritance.py
@@ -9,3 +9,4 @@ myChef.make_chicken() # prints "The chef makes chicken" from the class Chef
 myChineseChef = ChineseChef()
 myChineseChef.make_chicken() # prints "The chef makes chicken" from the class Chef
 myChineseChef.make_fried_rice() # prints "The chinese chef makes fried rice" from the class ChineseChef
+myChineseChef.make_special_dish()
--- Beginner/assets/chineseChef.py
+++ Beginner/assets/chineseChef.py
@@ -4,3 +4,6 @@ from assets.chef import Chef
 class ChineseChef(Chef):
     def make_fried_rice(self):
         print("The chinese chef makes fried rice")
+
+    def make_special_dish(self): # Overriding the method from the chef class
+        print("The chinese chef makes orange chicken")
