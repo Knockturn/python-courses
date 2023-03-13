97a2a21bdd25ab9f3398375f27fac1078cd0e4b2
Philippe Barka Kjellberg Bräuner <pkb@solar.dk>
2023-03-13 18:07:32 +0100

    32-progress

--- Beginner/32-Inheritance.py
+++ Beginner/32-Inheritance.py
@@ -1,1 +1,11 @@
 # https://www.youtube.com/watch?v=rfscVS0vtbw&t=15157s
+
+from assets.chef import Chef
+from assets.chineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken() # prints "The chef makes chicken" from the class Chef
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken() # prints "The chef makes chicken" from the class Chef
+myChineseChef.make_fried_rice() # prints "The chinese chef makes fried rice" from the class ChineseChef
---
+++ Beginner/assets/chineseChef.py
@@ -0,0 +1,6 @@
+# Inheritance
+# Gets all the methods and attributes from the chef class
+from chef import chef
+class ChineseChef(chef):
+    def make_fried_rice(self):
+        print("The chinese chef makes fried rice")
