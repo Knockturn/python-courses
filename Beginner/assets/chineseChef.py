b5a803d75d4635353e589fc4190e4d941731d6c0
Knockturn <pkbrauner@gmail.com>
2023-03-13 18:16:39 +0100

    32 fixed

--- Beginner/assets/chineseChef.py
+++ Beginner/assets/chineseChef.py
@@ -1,6 +1,6 @@
 # Inheritance
 # Gets all the methods and attributes from the chef class
-from chef import chef
-class ChineseChef(chef):
+from assets.chef import Chef
+class ChineseChef(Chef):
     def make_fried_rice(self):
         print("The chinese chef makes fried rice")
