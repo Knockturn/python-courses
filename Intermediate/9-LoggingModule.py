609b040a54adfc4e9fd784ffdc16c2689b001d97
Knockturn <pkbrauner@gmail.com>
2023-03-19 16:05:02 +0100

    debugger doneski

--- Intermediate/9-LoggingModule.py
+++ Intermediate/9-LoggingModule.py
@@ -1,4 +1,5 @@
 # The logging module is a standard library module that provides a simple interface to the logging system in Python.
+# Notice rotating file handler at the bottom.
 
 import logging
 # The logging module has 5 levels of logging:
@@ -26,5 +27,47 @@ logging.critical('This is a critical message')
 # This way, you can control the logging level in one place, and all the modules will use the same logging level.
 # Example:
 print('\n Importing the helper module...')
-import helper
+import helper # See Intermediate/helper.py
 helper.logger.debug('This is a debug message via the imported helper module')
+# See helper.py for writing to a file.
+helper.logger.error('This is an error message will be written to a file', exc_info=True) # exc_info=True will log the exception info.
+
+# logging.conf
+# The logging module can be configured using a configuration file.
+from os import path
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+# Above line gets the path to the current file, and then appends 'logging.conf' to the end of it.
+# Without it we would have to use an absolute path, which would not work on other computers.
+import logging.config
+logging.config.fileConfig(log_file_path)
+logger2 = logging.getLogger('simpleExample')
+logger2.debug('This is a debug message')
+
+# Rotating File Handler
+# The RotatingFileHandler class is used to create a file handler that will rotate the log file when it reaches a certain size.
+# Example:
+from logging.handlers import RotatingFileHandler
+logger3 = logging.getLogger('RotatingFileHandler')
+logger3.setLevel(logging.INFO)
+# Create a handler that will rotate the log file when it reaches 2KB.
+# The backupCount argument specifies how many backup files to keep.
+handler = RotatingFileHandler('rotating.log', maxBytes=2000, backupCount=5)
+logger3.addHandler(handler)
+for i in range(10000):
+    logger3.info('This is line %s' % i)
+
+# Timed Rotating File Handler
+# The TimedRotatingFileHandler class is used to create a file handler that will rotate the log file at certain timed intervals.
+# Example:
+from logging.handlers import TimedRotatingFileHandler
+import time
+logger4 = logging.getLogger('TimedRotatingFileHandler')
+logger4.setLevel(logging.INFO)
+# Create a handler that will rotate the log file every minute.
+handler = TimedRotatingFileHandler('timed.log', when='s', interval=2, backupCount=5)
+logger4.addHandler(handler)
+for i in range(3):
+    logger4.info('This is line %s' % i)
+    time.sleep(2)
+
+# Python json logger is a library that allows you to log in JSON format.
--- Intermediate/helper.py
+++ Intermediate/helper.py
@@ -1,4 +1,27 @@
 import logging
 
 logger = logging.getLogger(__name__)
+
+# You can stop propagation of log messages by setting the propagate attribute to False.
+# This will stop the log messages from being passed to the parent logger.
+# This is useful if you want to create a logger that only logs messages from a specific module.
+# Example:
+# logger.propagate = False
+
 logger.info('This is a message from inside the helper module')
+
+# create a file handler
+stream_h = logging.StreamHandler() # This will log to the console
+file_h = logging.FileHandler('file.log') # This will log to a file
+
+# level and format
+stream_h.setLevel(logging.WARNING) 
+file_h.setLevel(logging.ERROR) 
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
